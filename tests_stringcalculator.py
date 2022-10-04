611f0ba8dfe8115e0083e1add3308620574f4f60
José Abraham González Navarro <65511679+abrahamglez17@users.noreply.github.com>
2022-10-03 19:24:29 -0500

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -10,6 +10,9 @@ class TestStringMethods(unittest.TestCase):
     def test_one_args(self):
         self.assertEqual(stringcalculator.Add("1"), int(1))
 
+    def test_two_args(self):
+        self.assertEqual(stringcalculator.Add("2,3"), int(5))
+
 
 if __name__ == '__main__':
     unittest.main()
